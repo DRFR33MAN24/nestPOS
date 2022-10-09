8df53ebd780500b61cf16e9e5bd090ef6f8dd78c
Gordon Freeman <drfr33man@protonmail.com>
2022-10-09 05:36:26 +0300

    added core resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AdminsModule } from './admins/admins.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, AdminsModule, OrdersModule, PaymentsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
