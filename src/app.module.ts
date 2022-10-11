7c87248704fd3ad126fd720dc100f6aa120472d4
Gordon Freeman <drfr33man@protonmail.com>
2022-10-11 19:07:43 +0300

    added sequelize and graphql packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
@@ -7,9 +10,31 @@ import { AdminsModule } from './admins/admins.module';
 import { OrdersModule } from './orders/orders.module';
 import { PaymentsModule } from './payments/payments.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { join } from 'path';
 
 @Module({
-  imports: [ProductsModule, UsersModule, AdminsModule, OrdersModule, PaymentsModule, NotificationsModule],
+  imports: [
+    ProductsModule,
+    UsersModule,
+    AdminsModule,
+    OrdersModule,
+    PaymentsModule,
+    NotificationsModule,
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      models: [],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
