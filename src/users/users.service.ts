0b0a6371a4ee9f4e70a405a88d6a5de07455cf0e
Gordon Freeman <drfr33man@protonmail.com>
2022-10-12 19:39:31 +0300

    added models from original project

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,52 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable, Req } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './user.model';
 
-@Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
-}
 // @Injectable()
 // export class UsersService {
-//   constructor(
-//     @InjectModel(User)
-//     private userModel: typeof User,
-//   ) {}
-
-//   async findAll(): Promise<User[]> {
-//     return this.userModel.findAll();
+//   create(createUserDto: CreateUserDto) {
+//     return 'This action adds a new user';
 //   }
 
-//   findOne(id: string): Promise<User> {
-//     return this.userModel.findOne({
-//       where: {
-//         id,
-//       },
-//     });
+//   findAll() {
+//     return `This action returns all users`;
 //   }
 
-//   async remove(id: string): Promise<void> {
-//     const user = await this.findOne(id);
-//     await user.destroy();
+//   findOne(id: number) {
+//     return `This action returns a #${id} user`;
+//   }
+
+//   update(id: number, updateUserDto: UpdateUserDto) {
+//     return `This action updates a #${id} user`;
+//   }
+
+//   remove(id: number) {
+//     return `This action removes a #${id} user`;
 //   }
 // }
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+
+  async create(
+    @Body() createUserDto: CreateUserDto,
+    @Req() request,
+  ): Promise<User> {
+    return this.userModel.create(request.user.id, createUserDto);
+  }
+  async findAll(): Promise<User[]> {
+    return this.userModel.findAll();
+  }
+
+  findOne(id: string): Promise<User> {
+    return this.userModel.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: string): Promise<void> {
+    const user = await this.findOne(id);
+    await user.destroy();
+  }
+  async remove(id: string): Promise<void> {
+    const user = await this.findOne(id);
+    await user.destroy();
+  }
+}
